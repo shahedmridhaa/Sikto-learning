bd3421378549130600bf66a8624355a24d7323b2
shahedmridhaa <raselmridha9696@gmail.com>
2022-10-27 15:27:37 +0600

    blog question 1

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -9,7 +9,14 @@ const Blog = () => {
     return (
    <section className={`${darkMode ? "bg-white" : "bg-dark border-btm"}`}>
         <div className='container'>
-
+    <div class="card my-4">
+    <div class="card-header">
+    <b>What is cors</b>
+    </div >
+    <div class="card-body">
+    <p class="card-text">Cross-Origin Resource Sharing (CORS) is an HTTP-based security mechanism controlled and enforced by the client (web browser). It allows a service (API) to indicate any origin other than its own from which the client can request resources.</p>  
+      </div>
+      </div>
 
 </div>
    </section>
